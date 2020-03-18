daf0c7be31cb752b2dee56f7ac4c9cbb3c58ac19
BPierrick <pierrick.bellut95@gmail.com>
2020-03-18 13:03:00 +0100

    Changed the app max-width

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <AppProvider>
-        <Container maxWidth="xl">
+        <Container maxWidth="md">
           <TotalAmountComponent />
           <MapContainer />
           <AccountContainer />
